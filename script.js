2ff309e412275e026fd7702c8fae1b5bbcd8cd89
Diksha048 <bhagwatdiksha0408@gmail.com>
2024-08-12 10:02:27 +0530

    load from local storage  function added

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 //JS code
 const tasks=[];
 
-function loadTasks(){
-    localStorage.setItem('alltasks',JSON.stringify(tasks));
-    const div =document.getElementById("div");
-    div.innerHTML="";
-    for(const task of tasks){
-        div.innerHTML += `<div class="do-item> ${task}  <button class="delbtn" type = "button" onclick = "deleteTask('${task}')"> Delete </button> </div>`;
-    }
-}
+function loadFromLocalStorage(){
+    const allTasks = JSON.parse(localStorage.getItem("allTasks"));
 
-loadTasks();
+    if(allTasks){
+        tasks.push(...allTasks);
+    }
+
+    loadTasks();
+}
+loadFromLocalStorage();
+
+function loadTasks(){
+    localStorage.setItem("alltasks",JSON.stringify(tasks));
+
+    const tasksContainer =document.getElementById("div");
+    tasksContainer.innerHTML="";
+    for (const task of tasks) {
+        tasksContainer.innerHTML += `
+      <div class="todo-item">
+        ${task}
+        <button class="btn-del" type="button" onclick="deleteTask('${task}')">
+          Delete
+        </button>
+      </div>
+      `;
+      }
+    }
 
 function deleteTask(task)
 {
@@ -21,7 +38,7 @@ function deleteTask(task)
 }
 
 function add(){
-    const addtask =document.getElementById(addtask);
+    const addtask =document.getElementById("addtask");
     const addedtask = addtask.value;
     
     if(!addedtask){
